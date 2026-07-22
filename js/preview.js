7d3da20ffe594d3c32fcd86b92d3f984f01045a4
smzdlaw <shiuan604@gmail.com>
2026-07-22 14:15:54 +0800

    Restore preview before promissory template

--- js/preview.js
+++ js/preview.js
@@ -7,13 +7,34 @@ const Preview = {
     if (!paper) return;
 
     if (docType === 'payment-order') {
+
       paper.innerHTML = `<div class="preview-paper__inner">${PaymentOrderTemplate.render(formData)}</div>`;
+    
+    } if (docType === 'payment-order') {
+
+      paper.innerHTML = `
+        <div class="preview-paper__inner">
+          ${PaymentOrderTemplate.render(formData)}
+        </div>
+      `;
+    
     } else {
+    
       paper.innerHTML = `
         <div class="preview-paper__inner">
           <p class="preview-paper__placeholder">文件預覽將在此顯示</p>
         </div>
       `;
+    
+    }
+    } else {
+    
+      paper.innerHTML = `
+        <div class="preview-paper__inner">
+          <p class="preview-paper__placeholder">文件預覽將在此顯示</p>
+        </div>
+      `;
+    
     }
   }
 };
